c63abd2ec1569542aa02e38f75f40c13c1557782
KreivenWang <Kreiven.wang@hotmail.com>
2017-10-31 09:18:47 +0800

    单文件已合并, 使用git checkout branch_another path/to/file.txt

--- PumpDiagnosticsSystemV2/PumpDiagnosticsSystem/Business/SpectrumMessenger.cs
+++ PumpDiagnosticsSystemV2/PumpDiagnosticsSystem/Business/SpectrumMessenger.cs
@@ -69,6 +69,7 @@ namespace PumpDiagnosticsSystem.Business
                     autoAck: true,
                     consumer: consumer);
             }
+            //merge single file test
         }
     }
 }
